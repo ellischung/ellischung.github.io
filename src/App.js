f3ce82ead53e2e9d473c089b108f1b86020f1020
Ellis <ellischung98@gmail.com>
2022-01-27 18:20:59 -0500

    added text scramble effect for header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Resume from "./components/Resume";
 import About from "./components/About";
+import Scramble from "react-scramble";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       {/* Header */}
-      Ellis Chung
+      <Scramble
+        autoStart
+        text="ellis chung"
+        steps={[
+          {
+            roll: 10,
+            action: "+",
+            type: "all",
+          },
+          {
+            action: "-",
+            type: "forward",
+          },
+        ]}
+        style={{
+          color: "black",
+          fontSize: "32px",
+          fontWeight: "bold",
+          fontFamily: "Roboto Mono",
+        }}
+      />
       <Projects />
       <Resume />
       <About />
