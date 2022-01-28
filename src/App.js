de8146d57358651d3d8f44d911a60b8c280d87d4
Ellis <ellischung98@gmail.com>
2022-01-27 20:09:49 -0500

    added avatar and 2nd scramble text

--- src/App.js
+++ src/App.js
@@ -4,33 +4,77 @@ import Projects from "./components/Projects";
 import Resume from "./components/Resume";
 import About from "./components/About";
 import Scramble from "react-scramble";
+import Avatar from "@mui/material/Avatar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* Header */}
-      <Scramble
-        autoStart
-        text="ellis chung"
-        steps={[
-          {
-            roll: 10,
-            action: "+",
-            type: "all",
-          },
-          {
-            action: "-",
-            type: "forward",
-          },
-        ]}
-        style={{
-          color: "black",
-          fontSize: "32px",
-          fontWeight: "bold",
-          fontFamily: "Roboto Mono",
-        }}
-      />
+      <div className="header">
+        {/* Header */}
+        <Avatar
+          alt="ellis chung"
+          src={`${process.env.PUBLIC_URL}/temp.jpg`}
+          sx={{
+            my: 4,
+            width: 150,
+            height: 150,
+          }}
+        />
+        <Scramble
+          autoStart
+          text="ellis chung"
+          steps={[
+            {
+              roll: 20,
+              action: "+",
+              type: "all",
+            },
+            {
+              action: "-",
+              type: "forward",
+            },
+          ]}
+          style={{
+            marginBottom: "20px",
+            color: "black",
+            fontSize: "32px",
+            fontWeight: "bold",
+            fontFamily: "Roboto Mono",
+          }}
+        />
+        <Scramble
+          autoStart
+          text="software engineer"
+          steps={[
+            {
+              roll: 20,
+              action: "+",
+              type: "all",
+            },
+            {
+              action: "-",
+              type: "forward",
+            },
+            {
+              roll: 20,
+              action: "+",
+              type: "all",
+            },
+            {
+              action: "-",
+              type: "forward",
+            },
+          ]}
+          style={{
+            marginBottom: "20px",
+            color: "#515151",
+            fontSize: "20px",
+            fontWeight: "bold",
+            fontFamily: "Roboto Mono",
+          }}
+        />
+      </div>
       <Projects />
       <Resume />
       <About />
