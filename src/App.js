04aa96e7c87f3f997ad676cf7eca4dc999e5c284
Ellis <ellischung98@gmail.com>
2022-02-02 17:26:41 -0500

    edit about page, changed favicon ico

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
     <div className="App">
       <Navbar />
       <Header />
-      <Projects />
       <About />
+      <Projects />
       <Contact />
     </div>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div>
-           About Me 
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        textAlign: "center",
+      }}
+    >
+      <p
+        style={{
+          marginBottom: "40px",
+          fontSize: "24px",
+          fontWeight: "bold",
+          fontFamily: "Roboto Mono",
+        }}
+      >
+        About Me
+      </p>
+      <p
+        style={{
+          marginBottom: "100px",
+          color: "#727272",
+          fontFamily: "Open Sans",
+          width: "600px",
+        }}
+      >
+        My name is Ellis Chung and I am a graduate from Stony Brook University
+        with a B.S. in Computer Science. I am currently open to any tech
+        opportunities and would like to learn more about tech. My interest in
+        computer science started when I was a freshman in high school.
+      </p>
+    </div>
+  );
 };
 
 export default About;
