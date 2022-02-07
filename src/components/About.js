375c9c60896ea2b379a84bb82eef10b5eeac3789
Ellis <ellischung98@gmail.com>
2022-02-07 05:21:22 -0500

    changed about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,45 @@
 import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "row",
+    maxWidth: "69%",
+    maxHeight: 300,
+    borderRadius: "20px",
+    background: "#D8D8D8",
+    marginBottom: "100px",
+  },
+
+  content: {
+    background: "linear-gradient(to right, #D8D8D8 50%, white 50%)",
+    backgroundSize: "200% 100%",
+    backgroundPosition: "right bottom",
+    transition: "all .5s ease-out",
+    "&:hover": {
+      backgroundPosition: "left bottom",
+    },
+  },
+
+  text: {
+    padding: "22px",
+    color: "#727272",
+    fontFamily: "Open Sans",
+    transition: "all .6s ease-out",
+    "&:hover": {
+      color: "black",
+    },
+  },
+});
 
 const About = () => {
+  const classes = useStyles();
+
   return (
     <div
       style={{
@@ -20,37 +59,32 @@ const About = () => {
       >
         About Me
       </p>
-      <p
-        style={{
-          marginBottom: "100px",
-          color: "#727272",
-          fontFamily: "Open Sans",
-          width: "30%",
-        }}
-      >
-        My name is Ellis Chung and I am a graduate from Stony Brook University
-        with a B.S. in Computer Science. At first, I had little to no interest
-        in the field. During my sophomore year of high school, I was required to
-        select a major to focus the rest of my studies on. Nothing really
-        interested me at the time so I was completely stumped when the time came
-        for me to make a choice. I sifted through major after major, only to
-        find that I didn't want to pursue any of the career trajectories that
-        were offered. I figured that it was my ignorance that made me feel
-        indifferent so I took a shot in the dark and chose the major that was
-        the least uninteresting to me, which was software engineering. The first
-        class I took after getting into the major was web development. For one
-        of my projects in that class, I programmed Othello from scratch using
-        Javascript, HTML, and CSS. The satisfaction I felt afterwards changed my
-        outlook on the major and for the first time, the schoolwork I had didn't
-        feel like schoolwork because I genuinely enjoyed it. Debugging also
-        didn't feel dreadful because the feeling after finding a solution
-        outweighed any stress I would feel. From then on I went to tackle more
-        challenging projects and just like that, my journey to pursue a career
-        in computer science has started without me ever anticipating it...
-        Outside of programming, my hobbies are b-boying, building PCs, and
-        learning about new tech/hardware. I'm also currently studying 日本語.
-        (so I can watch anime without subtitles! jk lol)
-      </p>
+      <Card className={classes.root}>
+        <CardContent className={classes.content}>
+          <Typography className={classes.text}>
+            My name is Ellis Chung and I am a graduate from Stony Brook
+            University with a B.S. in Computer Science. At first, I had little
+            to no interest in the field. During my sophomore year of high
+            school, I was required to select a major to focus the rest of my
+            studies on. Nothing really interested me at the time so I was
+            completely stumped when the time came for me to make a choice. I
+            sifted through major after major, only to find that I didn't want to
+            pursue any of the career trajectories that were offered. I figured
+            that it was my ignorance that made me feel indifferent so I took a
+            shot in the dark and chose the major that was the least
+            uninteresting to me, which was software engineering. Outside of
+            programming, my hobbies are b-boying, playing video games, watching
+            anime, building PCs, and learning about new tech/hardware.
+          </Typography>
+        </CardContent>
+        <CardMedia
+          component="video"
+          width="500"
+          height="300"
+          src={`${process.env.PUBLIC_URL}/testvideo.mp4`}
+          controls
+        />
+      </Card>
     </div>
   );
 };
