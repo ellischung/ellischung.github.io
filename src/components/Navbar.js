aacaea3ded86e00e3fbc8e2d47650cccacbdca26
Ellis <ellischung98@gmail.com>
2022-03-15 18:21:04 -0400

    ready for deployment after changes to browser window resizing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
@@ -45,7 +44,7 @@ const Navbar = (props) => {
             sx={{
               margin: 0,
               padding: 0,
-              mr: 6,
+              mr: 5,
               color: "black",
               fontSize: "18px",
               fontWeight: "bold",
@@ -77,140 +76,142 @@ const Navbar = (props) => {
           >
             Home
           </Button>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            <Button
-              disableRipple
-              sx={{
-                margin: 0,
-                padding: 0,
-                mr: 2,
-                my: 2,
-                color: "black",
-                fontWeight: "bold",
-                fontFamily: "Roboto Mono",
-                textTransform: "none",
-                display: "inline-block",
-                position: "relative",
-                ":hover": {
-                  background: "transparent",
-                },
-                ":after": {
-                  content: '""',
-                  position: "absolute",
-                  width: "100%",
-                  transform: "scaleX(0)",
-                  height: "1px",
-                  bottom: 0,
-                  left: 0,
-                  backgroundColor: "black",
-                  transformOrigin: "bottom right",
-                  transition: "transform 0.25s ease-out",
-                },
-                ":hover:after": {
-                  transform: "scaleX(1)",
-                  transformOrigin: "bottom left",
-                },
-              }}
-              onClick={props.about}
-            >
-              About
-            </Button>
-            <Button
-              disableRipple
-              sx={{
-                margin: 0,
-                padding: 0,
-                mr: 2,
-                my: 2,
-                color: "black",
-                fontWeight: "bold",
-                fontFamily: "Roboto Mono",
-                textTransform: "none",
-                display: "inline-block",
-                position: "relative",
-                ":hover": {
-                  background: "transparent",
-                },
-                ":after": {
-                  content: '""',
-                  position: "absolute",
-                  width: "100%",
-                  transform: "scaleX(0)",
-                  height: "1px",
-                  bottom: 0,
-                  left: 0,
-                  backgroundColor: "black",
-                  transformOrigin: "bottom right",
-                  transition: "transform 0.25s ease-out",
-                },
-                ":hover:after": {
-                  transform: "scaleX(1)",
-                  transformOrigin: "bottom left",
-                },
-              }}
-              onClick={props.projects}
-            >
-              Projects
-            </Button>
-            <Button
-              disableRipple
-              sx={{
-                margin: 0,
-                padding: 0,
-                mr: 2,
-                my: 2,
-                color: "black",
-                fontWeight: "bold",
-                fontFamily: "Roboto Mono",
-                textTransform: "none",
-                display: "inline-block",
-                position: "relative",
-                ":hover": {
-                  background: "transparent",
-                },
-                ":after": {
-                  content: '""',
-                  position: "absolute",
-                  width: "100%",
-                  transform: "scaleX(0)",
-                  height: "1px",
-                  bottom: 0,
-                  left: 0,
-                  backgroundColor: "black",
-                  transformOrigin: "bottom right",
-                  transition: "transform 0.25s ease-out",
-                },
-                ":hover:after": {
-                  transform: "scaleX(1)",
-                  transformOrigin: "bottom left",
-                },
-              }}
-              onClick={props.contact}
-            >
-              Contact
-            </Button>
-          </Box>
-          <GitHubIcon
+          <Button
+            disableRipple
             sx={{
+              margin: 0,
+              padding: 0,
               mr: 2,
+              my: 2,
               color: "black",
-              fontSize: "30px",
-              cursor: "pointer",
-              "&:hover": { color: "white" },
+              fontWeight: "bold",
+              fontFamily: "Roboto Mono",
+              textTransform: "none",
+              display: "inline-block",
+              position: "relative",
+              ":hover": {
+                background: "transparent",
+              },
+              ":after": {
+                content: '""',
+                position: "absolute",
+                width: "100%",
+                transform: "scaleX(0)",
+                height: "1px",
+                bottom: 0,
+                left: 0,
+                backgroundColor: "black",
+                transformOrigin: "bottom right",
+                transition: "transform 0.25s ease-out",
+              },
+              ":hover:after": {
+                transform: "scaleX(1)",
+                transformOrigin: "bottom left",
+              },
             }}
-            onClick={() =>
-              window.open("https://github.com/ellischung", "_blank")
-            }
-          />
-          <EmailIcon
+            onClick={props.about}
+          >
+            About
+          </Button>
+          <Button
+            disableRipple
             sx={{
+              margin: 0,
+              padding: 0,
+              mr: 2,
+              my: 2,
               color: "black",
-              fontSize: "30px",
-              cursor: "pointer",
-              "&:hover": { color: "white" },
+              fontWeight: "bold",
+              fontFamily: "Roboto Mono",
+              textTransform: "none",
+              display: "inline-block",
+              position: "relative",
+              ":hover": {
+                background: "transparent",
+              },
+              ":after": {
+                content: '""',
+                position: "absolute",
+                width: "100%",
+                transform: "scaleX(0)",
+                height: "1px",
+                bottom: 0,
+                left: 0,
+                backgroundColor: "black",
+                transformOrigin: "bottom right",
+                transition: "transform 0.25s ease-out",
+              },
+              ":hover:after": {
+                transform: "scaleX(1)",
+                transformOrigin: "bottom left",
+              },
             }}
-            onClick={() => (window.location = "mailto:ellischung98@gmail.com")}
-          />
+            onClick={props.projects}
+          >
+            Projects
+          </Button>
+          <Button
+            disableRipple
+            sx={{
+              margin: 0,
+              padding: 0,
+              mr: 2,
+              my: 2,
+              color: "black",
+              fontWeight: "bold",
+              fontFamily: "Roboto Mono",
+              textTransform: "none",
+              display: "inline-block",
+              position: "relative",
+              ":hover": {
+                background: "transparent",
+              },
+              ":after": {
+                content: '""',
+                position: "absolute",
+                width: "100%",
+                transform: "scaleX(0)",
+                height: "1px",
+                bottom: 0,
+                left: 0,
+                backgroundColor: "black",
+                transformOrigin: "bottom right",
+                transition: "transform 0.25s ease-out",
+              },
+              ":hover:after": {
+                transform: "scaleX(1)",
+                transformOrigin: "bottom left",
+              },
+            }}
+            onClick={props.contact}
+          >
+            Contact
+          </Button>
+          <div style={{ marginLeft: "auto", paddingTop: "3px" }}>
+            <GitHubIcon
+              sx={{
+                mr: 2,
+                color: "black",
+                fontSize: "30px",
+                cursor: "pointer",
+                "&:hover": { color: "white" },
+              }}
+              onClick={() =>
+                window.open("https://github.com/ellischung", "_blank")
+              }
+            />
+            <EmailIcon
+              sx={{
+                color: "black",
+                fontSize: "30px",
+                cursor: "pointer",
+                "&:hover": { color: "white" },
+              }}
+              onClick={() =>
+                (window.location = "mailto:ellischung98@gmail.com")
+              }
+            />
+          </div>
         </Toolbar>
       </Container>
     </AppBar>
